5ee5e0f3ce187d535ff3f7b9090209b03bccbb11
Amshika2000 <amshika2000@gmail.com>
2021-10-06 21:32:10 +0530

    TSW-8 cloudinary image upload

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,26 +1,49 @@
 import Product from "../models/product.model";
+const cloudinary = require("../utils/cloudinary")
+const upload = require("../utils/multer");
+
 
 // @desc    Add a new product
 // @route   POST /api/products/
 // @access  Private/Admin
 const addProduct = async (req, res) => {
-	const title = req.body.title;
-	const price = req.body.price;
-	const category = req.body.category;
-	const description = req.body.description;
+// 	const title = req.body.title;
+// 	const price = req.body.price;
+// 	const category = req.body.category;
+// 	const description = req.body.description;
 
-	const newProduct = new Product({
-		title,
-		price,
-		category,
-		description,
-	});
+// 	const newProduct = new Product({
+// 		title,
+// 		price,
+// 		category,
+// 		description,
+// 	});
 
-	newProduct
-		.save()
-		.then(() => res.json("Product added!"))
-		.catch((err) => res.status(400).json("Error: " + err));
-};
+// 	newProduct
+// 		.save()
+// 		.then(() => res.json("Product added!"))
+// 		.catch((err) => res.status(400).json("Error: " + err));
+// };
+try {
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+     // Create new user
+    let product = new Product({
+	  title : req.body.title,
+	  price : req.body.price,
+	  category : req.body.category,
+	  description : req.body.description,
+      img: result.secure_url,
+      cloudinary_id: result.public_id,
+    });
+    // Save user
+    await product.save();
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+  }}; 
+
+
 
 // @desc    What does this do
 // @route   GET /api/products/
@@ -35,29 +58,61 @@ const getAllProducts = async (req, res) => {
 // @route   PUT /api/products/:id
 // @access  Permissions
 const updateProduct = async (req, res) => {
-	Product.findById(req.params.id)
-		.then((product) => {
-			product.title = req.body.title;
-			product.price = req.body.price;
-			product.category = req.body.category;
-			product.description = req.body.description;
+// 	Product.findById(req.params.id)
+// 		.then((product) => {
+// 			product.title = req.body.title;
+// 			product.price = req.body.price;
+// 			product.category = req.body.category;
+// 			product.description = req.body.description;
 
-			product
-				.save()
-				.then(() => res.json("product updated!"))
-				.catch((err) => res.status(400).json("Error: " + err));
-		})
-		.catch((err) => res.status(400).json("Error: " + err));
-};
+// 			product
+// 				.save()
+// 				.then(() => res.json("product updated!"))
+// 				.catch((err) => res.status(400).json("Error: " + err));
+// 		})
+// 		.catch((err) => res.status(400).json("Error: " + err));
+// };
+try {
+	let product = await Product.findById(req.params.id);
+	// Delete image from cloudinary
+	await cloudinary.uploader.destroy(product.cloudinary_id);
+	// Upload image to cloudinary
+	const result = await cloudinary.uploader.upload(req.file.path);
+	const data = {
+		title : req.body.title|| product.title,
+		price : req.body.price|| product.price,
+		category : req.body.category|| product.category,
+		description : req.body.description|| product.description,
+		img: result.secure_url || product.img,
+        cloudinary_id: result.public_id || product.cloudinary_id,
+	};
+	product = await Product.findByIdAndUpdate(req.params.id, data, {
+ new: true
+ });
+	res.json(product);
+  } catch (err) {
+	console.log(err);
+  }};
 
 // @desc    What does this do
 // @route   DELETE /api/products/:id
 // @access  Permissions
 const deleteProduct = async (req, res) => {
-	Product.findByIdAndDelete(req.params.id)
-		.then(() => res.json("product deleted."))
-		.catch((err) => res.status(400).json("Error: " + err));
-};
+// 	Product.findByIdAndDelete(req.params.id)
+// 		.then(() => res.json("product deleted."))
+// 		.catch((err) => res.status(400).json("Error: " + err));
+// };
+try {
+	// Find user by id
+	let product = await Product.findById(req.params.id);
+	// Delete image from cloudinary
+	await cloudinary.uploader.destroy(product.cloudinary_id);
+	// Delete user from db
+	await product.remove();
+	res.json(product);
+  } catch (err) {
+	console.log(err);
+  }};
 
 // @desc    What does this do
 // @route   GET /api/products/:id
--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 const router = Router();
+const upload = require("../utils/multer");
 import { auth } from "../middleware/auth";
 
 import {
@@ -10,11 +11,21 @@ import {
 	deleteProduct,
 } from "../controllers/products.controller";
 
-router.route("/").post(auth, addProduct).get(getAllProducts);
+// router.route("/").post(auth, addProduct).get(getAllProducts);
+// router
+// 	.route("/:id")
+// 	.put(auth, updateProduct)
+// 	.delete(auth, deleteProduct)
+// 	.get(getProduct);
+router.route("/").post(upload.single("image"), addProduct).get(getAllProducts);
+router.route("/:id").put(upload.single("image"), updateProduct);
+router.route("/:id").delete(upload.single("image"), deleteProduct);
+// router.put("/:id", upload.single("image"), async (req, res)
 router
 	.route("/:id")
-	.put(auth, updateProduct)
-	.delete(auth, deleteProduct)
+	.put(updateProduct)
+	.delete(deleteProduct)
 	.get(getProduct);
+
 
 export default router;
